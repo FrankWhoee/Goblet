efbd1bbf7e823cabed989e0b911406308f198b46
Frank Hui <huifrank@live.com>
2021-06-11 10:17:14 -0700

    Technically, all the features work now.

--- js/main.js
+++ js/main.js
@@ -1,56 +1,136 @@
- $('[data-toggle="tooltip"]').tooltip();
+$('[data-toggle="tooltip"]').tooltip();
 
-$(document).ready(function() {
-            socket = io()
-})
-
-function switchMode() {
-    $.ajax({
-        type: "GET",
-        url: '/switchmode',
-        success: function (response) {
-            location.reload();
-            Swal.fire({
-                position: 'top',
-                icon: 'success',
-                title: 'Switching mode',
-                showConfirmButton: false,
-                timer: 5000,
-                backdrop: false,
-                toast: true,
-                customClass: {
-                    border: '5px solid black'
-                }
-            })
-        },
-        error: function (response) {
-            Swal.fire({
-                position: 'top',
-                icon: 'error',
-                title: 'Something went wrong!',
-                showConfirmButton: false,
-                timer: 5000,
-                backdrop: false,
-                toast: true,
-                customClass: {
-                    border: '5px solid black'
-                }
-            })
+var socket = null
+var id = ""
+var ignoreNextSeek = false
+var ignoreNextPlay = false
+var ignoreNextPause = false
+$(document).ready(function () {
+    for (i = 0; i < 50; i++) {
+        id += String.fromCharCode(96 + Math.round(Math.random(25) * 25))
+    }
+    console.log(id)
+    socket = io()
+    socket.on('pause', function (message) {
+        retrieved_id = message
+        if (id !== retrieved_id) {
+            pause()
         }
-    });
-}
+    })
+    socket.on('play', function (message) {
+        retrieved_id = message
+        if (id !== retrieved_id) {
+            play()
+        }
+    })
+    socket.on('seek', function (message) {
+        time = message.split("-")[0]
+        retrieved_id = message.split("-")[1]
+        if (id !== retrieved_id) {
+            seek(parseInt(time))
+        }
+    })
+
+})
 
 var player = videojs("main")
 
-function seek(time){
+player.on('pause', function (event) {
+
+    if (!ignoreNextPause) {
+        $.ajax({
+            type: "GET",
+            url: '/sync?mode=pause',
+            success: function (response) {
+                Swal.fire({
+                    position: 'top',
+                    icon: 'success',
+                    title: 'Sync successful',
+                    showConfirmButton: false,
+                    timer: 2000,
+                    toast: true,
+                    customClass: {
+                        border: '5px solid black'
+                    }
+                })
+            },
+            error: function (response) {
+
+            }
+        });
+    }else{
+        ignoreNextPause = false
+    }
+})
+
+player.on('play', function (event) {
+
+    if (!ignoreNextPlay) {
+        $.ajax({
+            type: "GET",
+            url: '/sync?mode=play',
+            success: function (response) {
+                Swal.fire({
+                    position: 'top',
+                    icon: 'success',
+                    title: 'Sync successful',
+                    showConfirmButton: false,
+                    timer: 2000,
+                    toast: true,
+                    customClass: {
+                        border: '5px solid black'
+                    }
+                })
+            },
+            error: function (response) {
+
+            }
+        });
+        player.currentTime(player.currentTime());
+    }else{
+        ignoreNextPlay = false
+    }
+})
+
+player.on('seeked', function (event) {
+    if (!ignoreNextSeek) {
+        $.ajax({
+            type: "GET",
+            url: '/sync?mode=seek&time=' + player.currentTime().toString() + "&id=" + id,
+            success: function (response) {
+                Swal.fire({
+                    position: 'top',
+                    icon: 'success',
+                    title: 'Sync successful',
+                    showConfirmButton: false,
+                    timer: 2000,
+
+                    toast: true,
+                    customClass: {
+                        border: '5px solid black'
+                    }
+                })
+            },
+            error: function (response) {
+
+            }
+        });
+    }else{
+        ignoreNextSeek = false
+    }
+})
+
+function seek(time) {
     player.currentTime(time);
+    ignoreNextSeek = true
 }
 
-function pause(){
+function pause() {
     player.pause()
+    ignoreNextPause = true
 }
 
-function play(){
+function play() {
     player.play()
-
+    ignoreNextPlay = true
 }
--- main.py
+++ main.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 app.secret_key = 'jeV43V1KTG0ywgO1VGOdUfWzIiU51KoLYcrAIqdtpd7ukQC8LOKSgSjC2fvT'.encode('utf8')
 socketio = SocketIO(app, async_mode=None)
 
+
 @app.route('/assets/<path>')
 def send_assets(path):
     return send_from_directory('assets', path)
+
 
 @app.route('/css/<path>')
 def send_style(path):
@@ -19,38 +21,48 @@ def send_style(path):
 def send_js(path):
     return send_from_directory('js', path)
 
+
 @app.route('/videos/<path>')
 def send_videos(path):
     return send_from_directory('videos', path)
 
-@app.route('/pause')
-def pause():
-    print("pause")
 
-@app.route('/play')
-def play():
-    print("play")
+@app.route('/sync')
+def sync():
+    mode = request.args.get("mode")
+    id = request.args.get("id")
+    if mode == "pause":
+        broadcast("pause",id)
+    elif mode == "play":
+        broadcast("play",id)
+    elif mode == "seek":
+        try:
+            time = request.args.get("time")
+        except:
+            return "Requires time and ID for seeking."
+        broadcast("seek",time + "-" + id)
+    return Response(status=200)
 
-@app.route('/seek')
-def seek():
-    print("seek")
 
 @app.route('/')
 def index():
     return render_template('index.html',
                            sync_mode=socketio.async_mode)
 
+
 @socketio.on("connect")
 def connection():
     print("Connected a user.")
+
 
 @socketio.on('disconnect')
 def disconnect():
     print('A user disconnected')
 
-@socketio.on('my event')
-def broadcast(data):
-    emit('test broadcast', data, broadcast=True)
+
+def broadcast(type, data):
+    socketio.emit(type, data, broadcast=True)
+
 
 if __name__ == '__main__':
-    socketio.run(app,host="0.0.0.0", debug=True)
+    socketio.run(app, host="0.0.0.0", debug=True)
